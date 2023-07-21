d0ba991d093264796e8539f2bda7448f8fc4a9f7
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-21 20:29:13 -0300

    Aleatorio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import ComParametros from './componentes/basico/ComParametros';
 function App() {
   return (
     <div className="App">
-      
+
+      <Card titulo="Exercicio 3" color="#63ace5">
+        <Aleatorio min={1} max={10}/>
+      </Card>
+
       <Card titulo="Exercicio 2" color="#4a4e4d">
         <ComParametros nome='João' materia='Biologia' nota={6}/>
       </Card>
