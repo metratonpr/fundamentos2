10bc58f08b9fb0e3857d590df11de44b4540333e
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-21 19:58:15 -0300

    ComParametros

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Hello from './componentes/Hello';
+import Hello from './componentes/basico/Hello';
 import Card from './componentes/layout/Card';
 
 function App() {
   return (
     <div className="App">
-      <Card titulo="Exercicio 2" color="#011f4b">
+      <Card titulo="Exercicio 2" color="#4a4e4d">
         <h1> Exercicio 2</h1>
       </Card>
       <Card titulo="Exercicio 1 - Hello World" color="#2ab7ca">
---
+++ src/componentes/basico/ComParametros.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function ComParametros(props){
+
+}
+
+
+export default ComParametros
