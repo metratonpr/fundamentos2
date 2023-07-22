28b237bd3f88ce119fdae2adf591f083a74d834b
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-21 21:04:26 -0300

    Random

--- src/componentes/basico/Aleatorio.jsx
+++ src/componentes/basico/Aleatorio.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 export default function Aleatorio(props){
     const {min, max } = props;
+    const aleatorio = parseInt(Math.random() * (max - min)) + min;    
     return(
-
         <div>
             <h1>Numero Aleatorio</h1><br/>
             <h2>Min: {min}</h2><br/>
             <h2>Max: {max}</h2><br/>
+            <h2>Aleatorio: {aleatorio}</h2><br/>        
         </div>
-
     );
 }
