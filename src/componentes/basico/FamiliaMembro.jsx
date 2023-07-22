e1a4e109637a97e400cbee14cc42e2f7b02d6d42
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-21 21:47:05 -0300

    Familia Membro Sobrenome

--- src/componentes/basico/FamiliaMembro.jsx
+++ src/componentes/basico/FamiliaMembro.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function FamiliaMembro(props){
     return(
         <div>
-            <h1>Nome: {props.nome}</h1>
+            <h1>Nome: {props.nome} <strong>{props.sobrenome}</strong>.</h1>
         </div>
     )
 }
