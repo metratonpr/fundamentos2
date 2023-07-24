1458c9debd65403adaf6aee1b52fed7699c59c09
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-24 20:06:58 -0300

    Lista

---
+++ src/componentes/repeticao/Lista.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import alunos from '../../Data/alunos'
+
+export function Lista (props){
+    return (
+        <div>
+            <h1>Lista de Alunos</h1>
+        </div>
+    )
+}
