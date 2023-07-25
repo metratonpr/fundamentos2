f8beea92e2e27cd6229f709c3ab417bddd39dcfa
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-24 21:07:58 -0300

    Indireta

---
+++ src/componentes/indireto/IndiretoPai.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+
+export default function IndiretaPai() {
+
+    return (
+        <div>
+            <h2>Elemento Pai</h2>
+            <p>{nome}</p>
+            <p>{idade}</p>
+            <p>É nerd: {eNerd}</p>
+
+        </div>
+    )
+}
