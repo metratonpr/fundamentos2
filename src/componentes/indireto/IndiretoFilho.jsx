5df758bf213eaa6bd8969ad76cd94695d15b7a91
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-24 21:40:25 -0300

    onClick

--- src/componentes/indireto/IndiretoFilho.jsx
+++ src/componentes/indireto/IndiretoFilho.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function IndiretaFilho(props) {
     const clickPai = props.clickPai
+    
     const gerarIdade = () => parseInt(Math.random() * 100)
     const gerarNerd = () => Math.random() > 0.5
 
     return (
         <div>
             <div>Filho</div>
-            <button>
+            <button onClick={ 
+                (e) => clickPai("Joao", gerarIdade(), gerarNerd())}>
                 Fornecer Informações
             </button>
         </div>
