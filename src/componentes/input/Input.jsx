bafbde3cb6bdafd8b32f99672ce17b32220eb5a0
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-28 20:14:43 -0300

    Onchange

--- src/componentes/input/Input.jsx
+++ src/componentes/input/Input.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import './Input.css'
 
 export default function Input(props) {
+    const [valor, setValor] = useState('');
+
+    function quandoMudar(e){
+        setValor(e.target.value)
+    } 
     return (
         <div>
             <div>
                 <label for='inputA'>Input A</label>
-                <input type="text" id='inputA' />
+                <input type="text" id='inputA' value={valor} onChange={quandoMudar}/>
             </div>
             <div>
                 <label for='inputB'>Input B</label>
-                <input type="text" id='inputB' />
+                <input type="text" id='inputB' value={valor}/>
             </div>
         </div>
     );
